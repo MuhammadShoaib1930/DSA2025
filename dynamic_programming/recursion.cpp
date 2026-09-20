3266f05c011b80b8317805d6eddfb0cff70c4fe6
Muhammad shoaib <shoaibsn54321@gmail.com>
2026-09-20 23:43:31 +0500

    solved 42 dynamic programming questions

--- dynamic_programming/recursion.cpp
+++ dynamic_programming/recursion.cpp
@@ -10,6 +10,7 @@
 #include <queue>
 #include <algorithm>
 #include <vector>
+#include "my_algo.cpp"
 using namespace std;
 
 // Level 1 — Make recursion automatic
@@ -57,12 +58,10 @@ int digitSum(int n) {
 // Problem: Count how many digits are present in a number.
 // Input: 58392
 // Output: 5
-int pd(int n) {
-    if (n == 0)return 1;
-    return pd(n - 1) + 1;
-}
+//NOTE Again DO it
 int countDigit(int n) {
-    return (n < 0) ? pd(abs(n)) : pd(n);
+    if (n > -10 && n < 10)return 1;
+    return countDigit(n / 10) + 1;
 }
 // 6. Reverse a number
 // Problem: Reverse the digits of a given number.
@@ -77,10 +76,11 @@ int ReverseNumber(int n, int res = 0) {
 // Problem: Print all numbers starting from 1 up to n.
 // Input: 5
 // Output: 1 2 3 4 5
-void print1ToN(int n, int s = 1) {
-    if (s > n) return;
-    cout << s << " ";
+//NOTE again try it
+void print1ToN(int n) {
+    if (n < 1) return;
     print1ToN(n - 1);
+    cout << n << " ";
 }
 
 // 8. Print numbers n → 1
@@ -206,20 +206,16 @@ string ReverseString(string& str, int ind = 0, string res = "") {
 // Problem: Determine whether a string reads the same forward and backward.
 // Input: "madam"
 // Output: true
-bool isPalindromeString(string& str, int st = 0, int ed = -10) {
-    if (ed == -10) {
-        ed = str.size() - 1;
-    }
-    if (ed >= st)return true;
-    if (str[st] == str[ed]) {
-        return isPalindromeString(str, st + 1, ed - 1);
-    }
-    else {
-        return false;
-    }
+//NOTE again do it
+bool palindromePart(string str, int st, int ed) {
+    if (ed <= st)return true;
+    if (str[st] != str[ed])return false;
+    return palindromePart(str, st + 1, ed - 1);
 
 }
-
+bool isPalindromeString(string str) {
+    return palindromePart(str, 0, (str.size() - 1));
+}
 // 19. GCD using recursion
 // Problem: Find the greatest common divisor of two positive integers.
 // Input: 48, 18
@@ -302,7 +298,7 @@ void BinaryStrings(int n, vector<string>& result, string re = "", int id = 0) {
 // Input: arr = [1,2,3,4], r = 2
 // Output: [[1,2], [1,3], [1,4], [2,3], [2,4], [3,4]]
 
-void Combinations(vector<int>& arr, int r, vector<vector<int>>& result, vector<int> res = {}, int ind=0) {
+void Combinations(vector<int>& arr, int r, vector<vector<int>>& result, vector<int> res = {}, int ind = 0) {
     if (res.size() >= r) {
         result.push_back(res);
         return;
@@ -329,7 +325,7 @@ void Permutations(vector<int>& arr, vector<vector<int>>& result, int ind = 0) {
     for (int i = ind; i < arr.size(); i++)
     {
         swap(arr[i], arr[ind]);
-        Permutations(arr, result, ind+1);
+        Permutations(arr, result, ind + 1);
         swap(arr[i], arr[ind]);
     }
 
@@ -339,44 +335,146 @@ void Permutations(vector<int>& arr, vector<vector<int>>& result, int ind = 0) {
 // Problem: Each digit from 2 to 9 represents letters on a phone keypad. Generate all possible letter combinations.
 // Input: "23"
 // Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
- 
+string latters(char ch) {
+    switch (ch)
+    {
+    case '2':
+        return "abc";
+    case '3':
+        return "def";
+    case '4':
+        return "ghi";
+    case '5':
+        return "hkl";
+    case '6':
+        return "mno";
+    case '7':
+        return "pqrs";
+    case '8':
+        return "tuv";
+    case '9':
+        return "wxyz";
+    default:
+        return "";
+    }
+}
+void combinationsPhoneNumber(string& str, vector<string>& result, string res = "", int ind = 0) {
+    if (ind >= str.size()) {
+        result.push_back(res);
+        return;
+    }
+    string s = latters(str[ind]);
+    for (int i = 0; i < s.size(); i++)
+    {
+        res += s[i];
+        combinationsPhoneNumber(str, result, res, ind + 1);
+        res.pop_back();
+    }
+}
 
 // 27. Subset Sum
 // Problem: Generate the sums of all possible subsets of the given array.
 // Input: [1, 2, 3]
 // Output: [0, 1, 2, 3, 3, 4, 5, 6]
+void SubsetSum(vector<int>& arr, vector<int>& result, int res = 0, int ind = 0) {
+    if (ind >= arr.size()) {
+        result.push_back(res);
+        return;
+    }
+    res += arr[ind];
+    SubsetSum(arr, result, res, ind + 1);
+    res -= arr[ind];
+    SubsetSum(arr, result, res, ind + 1);
+}
 
 // Level 4 — Recurrence-thinking problems
 // 28. Elimination Game
 // Problem: Start with numbers 1 to n. Repeatedly eliminate every second number from alternating directions. Find the last remaining number.
 // Input: n = 9
 // Output: 6
+int Elimination(int n, int head = 1, int step = 1, bool left = true)
+{
+    if (n == 1)
+        return head;
+    if (left || (n % 2 == 1))
+        head += step;
+
+    step *= 2;
+    n /= 2;
+
+    return Elimination(n, head, step, !left);
+}
+
 
 // 29. Josephus Problem
 // Problem: n people stand in a circle. Starting from the first person, eliminate every k-th person until one remains.
 // Input: n = 5, k = 2
 // Output: 3
+// Note again do it
+int JosephusPart(int n, int k) {
+    if (n == 1)return 0;
+    return (JosephusPart(n - 1, k) + k) % n;
+}
+int Josephus(int n, int k) {
+    return JosephusPart(n, k) + 1;
+}
 
 // 30. Tower of Hanoi
 // Problem: Move all disks from one tower to another while following the Tower of Hanoi rules.
 // Input: n = 3
 // Output: 7 moves
+//NOTE again do it
+int towerOfHanoi(int n) {
+    if (n < 1)return 0;
+    int r = towerOfHanoi(n - 1);
+    return r + r + 1;
+}
 
 // 31. K-th symbol in grammar
 // Problem: A binary grammar generates rows recursively. Find the symbol at position k in row n.
 // Input: n = 4, k = 5
 // Output: 1
+//NOTE again do it
+bool K_thSymbolInGrammer(int n, int k) {
+    if (n == 1)return 0;
+    int half = power(2, n - 2);
+    if (k <= half)
+        return K_thSymbolInGrammer(n - 1, k);
+    return !K_thSymbolInGrammer(n - 1, k - half);
+}
 
 // 32. K-th character in string
 // Problem: A string is repeatedly transformed according to a recursive rule. Find the character at position k.
 // Input: n = 3, k = 5
 // Output: b
+//NOTE again do it
+char kthCharacter(int n, int k) {
+    if (n == 1 || k == 1)return 'a';
+    int half = power(2, n - 2);
+    if (k > half) {
+        return kthCharacter(n - 1, k - half) + 1;
+    }
+    return kthCharacter(n - 1, k);
+}
 
 // 33. Recursive binary-number problems
 // Problem: Generate or analyze binary numbers recursively according to the given length or position.
 // Input: n = 3
 // Output: 000, 001, 010, 011, 100, 101, 110, 111
-
+void binary_number(int n, vector<string>& result, string res = "") {
+    if (res.size() == n) {
+        result.push_back(res);
+        return;
+    }
+    if (res.size() > n) {
+        return;
+    }
+    res += "0";
+    binary_number(n, result, res);
+    res.pop_back();
+    res += "1";
+    binary_number(n, result, res);
+}
 // 34. Divide/reduce problems
 // Problem: Solve a problem by repeatedly reducing the input into a smaller problem until reaching a base case.
 // Input: n = 16
@@ -388,58 +486,149 @@ void Permutations(vector<int>& arr, vector<vector<int>>& result, int ind = 0) {
 // Problem: You can climb either 1 or 2 stairs at a time. Find how many different ways can reach the top.
 // Input: n = 5
 // Output: 8
-
+int ClimbingStairs(int n) {
+    if (n <= 2)return n;
+    return ClimbingStairs(n - 1) + ClimbingStairs(n - 2);
+}
 // 36. Frog Jump
 // Problem: A frog starts at the first stone and can jump to the next stone or skip one stone. Find the minimum cost to reach the last stone.
 // Input: heights = [10, 30, 20, 40]
 // Output: 30
+//NOTE again do it
+int ForgJumb(vector<int>& arr, int n = 0) {
 
+    if (n >= arr.size())return 0;
+    return min(ForgJumb(arr, n + 1), ForgJumb(arr, n + 2)) + arr[n];
+}
 // 37. House Robber
 // Problem: Rob houses to maximize money, but you cannot rob two adjacent houses.
 // Input: [2, 7, 9, 3, 1]
 // Output: 12
+// NOTE again do it
+int HouseRobberPart(vector<int>& arr, int n = 0, bool istake = false) {
+    if (n >= arr.size())return 0;
+    for (int i = n; i < arr.size(); i++) {
+        int a = 0, b = 0;
+        if (istake) {
+            a = HouseRobberPart(arr, n + 1, !istake);
+        }
+        else {
+            b = HouseRobberPart(arr, n + 1, !istake) + arr[n];
 
+        }
+        return max(a, b);
+    }
+    return 0;
+}
+int HouseRobber(vector<int>& arr) {
+    return max(HouseRobberPart(arr, 0), HouseRobberPart(arr, 1));
+}
 // 38. 0/1 Knapsack
 // Problem: Choose items with given weights and values without exceeding the maximum capacity. Each item can be chosen at most once.
 // Input: weights=[2,3,4], values=[4,5,7], capacity=5
 // Output: 9
+//NOTE again do it
+int knapsack(vector<int>& weights, vector<int>& values, int capacity, int ind = 0) {
+    if (capacity == 0)return 0;
+    if (ind >= weights.size())return INT_MIN;
+    if (capacity < 0)return INT_MIN;
+    int v1 = knapsack(weights, values, capacity - weights[ind], ind + 1) + values[ind];
+    int v2 = knapsack(weights, values, capacity, ind + 1);
+    return max(v1, v2);
+}
+
 
 // 39. Coin Change
 // Problem: Find the minimum number of coins needed to make the given amount.
 // Input: coins=[1,2,5], amount=11
 // Output: 3
+// NOTE again do it
+int CoinChange(vector<int>& arr, int amount) {
+    if (amount == 0)return 0;
+    int ans = INT_MAX;
+    if (amount < 0)return INT_MAX;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        int subAns = CoinChange(arr, amount - arr[i]);
+        if (subAns != INT_MAX)
+            ans = min(ans, subAns) + 1;
+    }
+    return ans;
+}
 
 // 40. Unique Paths
 // Problem: A robot moves only right or down in a grid. Find the number of different paths from the top-left to the bottom-right.
 // Input: m = 3, n = 3
 // Output: 6
+// NOTE do it again
+int UniquePaths(int n, int m) {
+    if (n == 1)return 1;
+    if (m == 1)return 1;
+    return UniquePaths(n, m - 1) + UniquePaths(n - 1, m);
+}
 
 // 41. Longest Common Subsequence
 // Problem: Find the length of the longest subsequence that appears in both strings in the same order.
 // Input: text1="abcde", text2="ace"
 // Output: 3
+// NOTE do it again
+int LCS(string& str1, string& str2, int n1 = 0, int n2 = 0) {
+    if (n1 >= str1.size())return 0;
+    if (n2 >= str2.size())return 0;
+
+    if (str1[n1] == str2[n2]) {
+        return LCS(str1, str2, n1 + 1, n2 + 1) + 1;
+    }
+    return max(LCS(str1, str2, n1 + 1, n2), LCS(str1, str2, n1, n2 + 1));
+}
 
 // 42. Longest Increasing Subsequence
 // Problem: Find the length of the longest subsequence whose values are strictly increasing.
 // Input: [10,9,2,5,3,7,101,18]
 // Output : 4
+// NOTE do it again
+int LIS(vector<int>& arr, int ind = 0, int prevous = 0) {
+    if (ind >= arr.size())return 0;
+    int v1 = LIS(arr, ind + 1, prevous);
+    int v2 = 0;
+    if (prevous < arr[ind]) {
+        v2 = LIS(arr, ind + 1, arr[ind]) + 1;
+    }
+    return max(v1, v2);
+}
+
 
 int main() {
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
-    vector<int> arr = { 1,2,3 };
-    vector<vector<int>> result;
-    Permutations(arr, result);
-    for (auto&& i : result)
-    {
-        for (auto&& j : i)
-        {
-            cout << j << " ";
-        }
-        cout << endl;
-    }
+    // vector<int> arr = inputVector<int>();
+    // cout << longestIncressing(arr);
 
-
+    // cout << countDigit(10000001);
+    // print1ToN(10);
+    // cout << isPalindromeString("abba");
+    // cout << towerOfHanoi(4) << endl;
+    // cout << ForgJumb(arr);
+    // cout << HouseRobber(arr);
+    // cout << CoinChange(arr, a);
+    // cout << kthCharacter(n, k) << endl;
+    // vector<int> arr = inputVector<int>();
 
     return 0;
 }
+
+// Count digits
+// Print 1 → n
+// Palindrome
+// Tower of Hanoi
+// Frog Jump
+// House Robber
+// Coin Change
+
+// Round 2 — Solve without looking at your previous code
+// Josephus
+// K-th Symbol Grammar
+// K-th Character
+// 0/1 Knapsack
+// Unique Paths
+// LCS
